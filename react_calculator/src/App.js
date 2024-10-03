8a8a64537763722842fc900f75ce4338e633e4eb
Egor Nesterov <egor.nesterov2002@gmail.com>
2024-10-03 05:42:15 +0200

    Test commit

---
+++ react_calculator/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className='container'>
+      <div className='calculator'>
+        <form action="">
+          <div>
+            <input type = "text"></input>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
